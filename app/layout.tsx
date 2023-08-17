5ec509d2bf40339fa15e7e9ea4c2312b15eac5cc
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-18 04:16:37 +0530

    Defined Header & Transition

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -16,10 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left[-28rem] xl:left[-15rem] 2xl:left-[-6rem]"></div>
 
+        <Header />
         {children}
       </body>
     </html>
