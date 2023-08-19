8cbbe60c6d88a376ebac0c0787e4831553f513dc
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-20 04:35:50 +0530

    Divider added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Divider from "@/components/divider";
 import Intro from "@/components/intro";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <Divider />
     </main>
   )
 }
